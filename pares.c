d6f3cfb71bd920ed094439c0823e98dcc569e790
harrueds <harrue.ds@gmail.com>
2023-08-13 01:49:35 -0400

    modified:   pares.c

--- pares.c
+++ pares.c
@@ -2,5 +2,15 @@
 int main()
 {
     int a,b;
-    printf("\nIngrese dos números eteros")
+    printf("\nIngrese dos números enteros");
+    scanf("%i %i",&a,&b);
+    if(a%2 == 0 && b%2 == 0)
+    {
+        printf("\nLos dos números son pares\n\n");
+    }
+    else
+    {
+        printf("\nUno de los números no es par");
+    }
+    return 0;
 }
